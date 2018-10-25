05bba28f744afd0ec3ddfbe965d765fe23646e58
Gurinder Batth <gurinder@gmx.com>
2018-10-24 17:06:28 -0700

    Second commit for this project

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//  First question.
 var userCountry = prompt ('What country was I born in?').toLocaleUpperCase();
 console.log ('To check if the input was capitalized', userCountry);
 if(userCountry === 'IN' || userCountry ==='INDIA') {
@@ -7,6 +8,7 @@ else {
   alert('I am surprised you don\'t know');
 }
 
+//  Second question.
 var userState = prompt ('What state am I from in U.S.?').toLocaleUpperCase();
 console.log ('To check if the input was capitalized', userState);
 if (userState === 'MO' || userState ==='MISSOURI') {
@@ -16,6 +18,7 @@ else {
   alert('Nope not my state');
 }
 
+//  Third question.
 var userCusine = prompt ('What is my favourite cusine? Guess if it is Italian or Chinese or . . . . ').toLocaleUpperCase();
 console.log ('To check if the input was capitalized', userCusine);
 if (userCusine === 'INDIAN') {
@@ -25,6 +28,7 @@ else {
   alert('It should have been an easy guess too but . . . . ');
 }
 
+//  Forth question.
 var userHobby = prompt ('What is my favourite hobby. Let me make it simple, it is a board game :)').toLocaleUpperCase();
 console.log ('To check if the input was capitalized', userHobby);
 if (userHobby === 'CHESS') {
@@ -34,8 +38,34 @@ else {
   alert('You ***k! at guessing');
 }
 
+//  Fifth question.
 var userJob = prompt('What was my last job?').toLocaleUpperCase();
 console.log ('To check if the input was capitalized', userJob);
-if (userJob === 'IT' || userJob === 'COMPUTERS' || userJob === 'INFORMATION TECHNOLOGY' || userJob === 'COMPUTER SCIENCE');{
+if (userJob === 'IT' || userJob === 'COMPUTERS' || userJob === 'INFORMATION TECHNOLOGY' || userJob === 'COMPUTER SCIENCE') {
   alert('Who could have guessed!');
 }
+else {
+  alert('It was an easy guess!');
+}
+
+//  Sixsth question.
+var guessNum = parseInt(prompt('Guess how many years I have been in U.S.! It is between 8 - 20 years. You only get 4 tries'));
+console.log ('To check if the input was parsed to integer');
+var counter = parseInt('1');
+while (guessNum !== 12) {
+  if (guessNum < 12 && counter < 4) {
+    guessNum = parseInt(prompt('Your guess is low, guess again'));
+    counter ++;
+  }
+  else if (guessNum > 12 && counter < 4) {
+    guessNum = parseInt(prompt('Your guess is High, guess again'));
+    counter ++;
+  }
+
+}
+if (guessNum === 12){
+  alert('Good guess! I can\'t believe I have been in United States for ' + guessNum + ' years.');
+}
+
+//Seventh question
+
