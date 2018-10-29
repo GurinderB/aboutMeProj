4afb256db818b9889afec5e0122ed5d3c288235e
Gurinder Batth <gurinder@gmx.com>
2018-10-28 21:54:06 -0700

    Added HTML, CSS and fixed the question 6 & 7

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This repo will hold all the info for the about me project
 Driver on project: Nate Tibbals
 Navigator: Gurinder Batth
+
+
--- app.js
+++ app.js
@@ -1,113 +1,124 @@
+"use strict";
+
+var numCorrect = 0;
 //  First question.
-function guessCountry() {
-  var userCountry = prompt('What country was I born in?').toLocaleUpperCase();
-  console.log('To check if the input was capitalized', userCountry); if (userCountry === 'IN' || userCountry === 'INDIA') {
-    return alert('It is a dead givaway but I am glad you know that I am from ' + userCountry + '.');
-  }
-  else {
-    return alert('I am surprised you don\'t know');
-  }
+var userCountry = prompt('What country was I born in?').toLocaleUpperCase();
+console.log('To check if the input was capitalized', userCountry);
+if (userCountry === 'IN' || userCountry === 'INDIA') {
+  alert('It is a dead givaway but I am glad you know that I am from ' + userCountry + '.');
+  numCorrect++;
 }
-guessCountry();
+else {
+  alert('I am surprised you don\'t know');
+
+}
+
 //  Second question.
-function guessUserState() {
-  var userState = prompt('What state am I from in U.S.?').toLocaleUpperCase();
-  console.log('To check if the input was capitalized', userState);
-  if (userState === 'MO' || userState === 'MISSOURI') {
-    return alert('Good guess that I am from ' + userState + '.');
-  }
-  else {
-    return alert('Nope not my state');
-  }
+var userState = prompt('What state am I from in U.S.?').toLocaleUpperCase();
+console.log('To check if the input was capitalized', userState);
+if (userState === 'MO' || userState === 'MISSOURI') {
+  alert('Good guess that I am from ' + userState + '.');
+  numCorrect++;
 }
-guessUserState();
+else {
+  alert('Nope not my state');
+
+}
+
 //  Third question.
-function guessCuisine() {
-  var userCusine = prompt('What is my favourite cusine? Guess if it is Italian or Chinese or . . . . ').toLocaleUpperCase();
-  console.log('To check if the input was capitalized', userCusine);
-  if (userCusine === 'INDIAN') {
-    return alert('Right on! But again an easy guess that ' + userCusine + ' is my favourite cusine');
-  } else {
-    return alert('It should have been an easy guess too but . . . . ');
-  }
+var userCusine = prompt('What is my favourite cusine? Guess if it is Italian or Chinese or . . . . ').toLocaleUpperCase();
+console.log('To check if the input was capitalized', userCusine);
+if (userCusine === 'INDIAN') {
+  alert('Right on! But again an easy guess that ' + userCusine + ' is my favourite cusine');
+  numCorrect++;
 }
-guessCuisine();
+else {
+  alert('It should have been an easy guess too but . . . . ');
+
+}
+
 //  Forth question.
-function guessHobby() {
-  var userHobby = prompt('What is my favourite hobby. Let me make it simple, it is a board game :)').toLocaleUpperCase();
-  console.log('To check if the input was capitalized', userHobby);
-  if (userHobby === 'CHESS') {
-    return alert('You are amazing at guessing');
-  }
-  else {
-    return alert('You ***k! at guessing');
-  }
+var userHobby = prompt('What is my favourite hobby. Let me make it simple, it is a board game :)').toLocaleUpperCase();
+console.log('To check if the input was capitalized', userHobby);
+if (userHobby === 'CHESS') {
+  alert('You are amazing at guessing');
+  numCorrect++;
 }
-guessHobby();
+else {
+  alert('You ***k! at guessing');
+}
+
 //  Fifth question.
-function guessJob() {
-  var userJob = prompt('What was my last job?').toLocaleUpperCase();
-  console.log('To check if the input was capitalized', userJob);
-  if (userJob === 'IT' || userJob === 'COMPUTERS' || userJob === 'INFORMATION TECHNOLOGY' || userJob === 'COMPUTER SCIENCE') {
-    return alert('Who could have guessed!');
-  }
-  else {
-    return alert('It was an easy guess!');
-  }
+var userJob = prompt('What was my last job?').toLocaleUpperCase();
+console.log('To check if the input was capitalized', userJob);
+if (userJob === 'IT' || userJob === 'COMPUTERS' || userJob === 'INFORMATION TECHNOLOGY' || userJob === 'COMPUTER SCIENCE') {
+  alert('Who could have guessed!');
+  numCorrect++;
+}
+else {
+  alert('It was an easy guess but you missed!');
 }
 
-guessJob();
-//  Sixsth question.
-function guessNumber() {
-  var guessNum = parseInt(prompt('Guess how many years I have been in U.S.! It is between 8 - 20 years. You only get 4 tries'));
-  console.log('To check if the input was parsed to integer');
-  var counter = 1;
-  while (guessNum !== 12 && counter < 4) {
-    if (guessNum < 12) {
-      counter++;
-      return guessNum = parseInt(prompt('Your guess is low, guess again'));
-    }
-    else if (guessNum > 12) {
-      counter++;
-      return guessNum = parseInt(prompt('Your guess is High, guess again'));
-    }
-guessNumber();
+//  Sixth question.
+var guessNum = parseInt(prompt('Guess how many years I have been in U.S.! It is between 8 - 20 years. You only get 4 tries'));
+console.log('To check if the input was parsed to integer');
+//if (guessNum === null) {guessNum = parseInt(prompt('Enter a number!'));}
 
+console.log('To check if value came in as null', guessNum);
+
+var counter = 1;
+while (guessNum !== 12 && counter < 4) {
+  if (guessNum < 12) {
+    guessNum = parseInt(prompt('Your guess is low, guess again'));
+    counter++;
   }
-  if (guessNum === 12) {
-    return alert('Good guess! I can\'t believe I have been in United States for ' + guessNum + ' years.');
+  else if (guessNum > 12) {
+    guessNum = parseInt(prompt('Your guess is High, guess again'));
+    counter++;
   }
   else {
-    return alert('You are out of tries!');
+    guessNum = parseInt(prompt('Enter a number, try again'));
+    counter++;
   }
 }
-guessNumber();
+if (guessNum === 12) {
+  alert('Good guess! I can\'t believe I have been in United States for ' + guessNum + ' years.');
+}
+else {
+  alert('You are out of tries!');
+}
 
 //Seventh question
 
-function guessMyCountry() {
-  var guessCountry = prompt('Guess the countries I have visited other than India or U.S.!').toLocaleUpperCase();
-  console.log('To check if the input was converted to uppercase', guessCountry);
-  var countryVis = ['NETHERLAND', 'CYPRUS', 'BELGIUM'];
-  var correct = false;
-  var counter2 = 0;
-  while (counter2 <= 5) {
-    for (var i = 0; i <= countryVis.length; i++) {
-      if (guessCountry === countryVis[i]) {
-        correct = true;
-      }
-    }
-    if (correct === true) {
-      alert('Good guess!');
-      break;
-    }
-    else {
-      guessCountry = prompt(' Wrong guess, try again. You get to try for 7 time total.').toLocaleUpperCase();
-      counter2++;
+var guessCountry = prompt('Guess the countries I have visited other than India or U.S.!').toLocaleUpperCase();
+console.log('To check if the input was converted to uppercase', guessCountry);
+var countryVis = ['NETHERLAND', 'CYPRUS', 'BELGIUM'];
+var correct = false;
+var counter2 = 0;
+while (counter2 <= 4) {
+  for (var i = 0; i <= countryVis.length; i++) {
+    if (guessCountry === countryVis[i]) {
+      correct = true;
     }
   }
-  if (counter2 === 6) {
-    alert('You are out of tries');
+  if (correct === true) {       // you can also use if(correct) for 'true' value of the boolian 'correct' also can use if(!correct).
+    alert('Good guess!');
+    numCorrect++;
+    break;
+  }
+  else {
+    guessCountry = prompt(' Wrong guess, try again. You get to try for 7 time total.').toLocaleUpperCase();
+    counter2++;
   }
 }
-guessMyCountry();
+if (counter2 === 6) {
+  alert('You are out of tries');
+
+}
+
+// to check number of correct answers
+if (numCorrect > 4) {
+  alert('You got ' + numCorrect + ' out of 7 questions. Great Job!'); //alert if total correct greater than 4
+} else {
+  alert('You only got ' + numCorrect + ' out of 7 questions');
+}
